ffcb75493e1551451d211acdb58bd8b030e35d56
Do Minh Kha <kha.minhdo199@gmail.com>
2019-09-18 23:12:03 +0700

    kha - prepare for project

--- src/main/java/net/springteam/CarRenting/CarRentingApplication.java
+++ src/main/java/net/springteam/CarRenting/CarRentingApplication.java
@@ -2,10 +2,14 @@ package net.springteam.CarRenting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CarRentingApplication {
 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(CarRentingApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CarRentingApplication.class, args);
 	}
